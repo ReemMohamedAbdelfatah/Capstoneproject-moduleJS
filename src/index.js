f7fb827c21f2a7c6c373062080fe9364ee36e260
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-01-04 12:07:03 +0200

    Try Consume PokeApi

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import style from './style.css'
+const pokeList = document.querySelector('#pokeList');
+const fetchPokemon = () => {
+  const promises = [];
+  for(let i = 1; i<= 150; i++)
+  {
+  const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+  promises.push(fetch(url).then(res =>  res.json()));
+  }
+  Promise.all(promises).then( results => {
+    const pokemon = results.map( result => ({
+      name: result.name,
+      id: result.id,
+      image: result.sprites['front_default'],
+      type: result.types.map((type) => type.type.name).join(', ')
+    }));
+    displayPokemon(pokemon);
+  });
+
+};
+
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon.map( pokeman => `
+  <li class="card">
+     <img class="card-image" src="${pokeman.image}">
+     <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+     <p class="card-subtitle">Type: ${pokeman.type}</p>
+  </li>`);
+  pokeList.innerHTML = pokemonHTMLString;
+}
+
+fetchPokemon();
