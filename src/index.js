44cf4796acdaf65e281d775119b2181458ded3b3
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-01-04 21:51:56 +0200

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import style from './style.css'
+/* eslint-disable  no-unused-vars  , no-use-before-define , no-plusplus */
+import style from './style.css';
+
 const pokeList = document.querySelector('#pokeList');
 const fetchPokemon = () => {
   const promises = [];
-  for(let i = 1; i<= 150; i++)
-  {
-  const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-  promises.push(fetch(url).then(res =>  res.json()));
+  for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
   }
-  Promise.all(promises).then( results => {
-    const pokemon = results.map( result => ({
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((result) => ({
       name: result.name,
       id: result.id,
-      image: result.sprites['front_default'],
-      type: result.types.map((type) => type.type.name)
+      image: result.sprites.front_default,
+      type: result.types.map((type) => type.type.name),
     }));
     displayPokemon(pokemon);
   });
-
 };
 
 const displayPokemon = (pokemon) => {
-  const pokemonHTMLString = pokemon.map( pokeman => `
+  const pokemonHTMLString = pokemon.map((pokeman) => `
   <li class="card">
      <img class="card-image" src="${pokeman.image}">
      <h2 class="card-title">${pokeman.id}. ${pokeman.name}<button id="like"><i class="number-likes"></i><i class="fa fa-heart"></i></button></h2>
@@ -29,6 +29,6 @@ const displayPokemon = (pokemon) => {
      <button id="reservationBtn">Reserve</button>
   </li>`).join('');
   pokeList.innerHTML = pokemonHTMLString;
-}
+};
 
 fetchPokemon();
