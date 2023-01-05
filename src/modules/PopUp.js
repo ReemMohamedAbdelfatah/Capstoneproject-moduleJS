b9fc9e1c9a103d3bb8680625b7ef53afe28cd9ff
tsohleDev <mokhemisitsohle@gmail.com>
2023-01-05 05:21:40 +0200

    Call Involvement to get comments

--- src/modules/PopUp.js
+++ src/modules/PopUp.js
@@ -1,6 +1,8 @@
 import './PopUp.css';
 
 class PopUp {
+  static url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DoParOktO7RAA0Mhxygs/comments'
+
   static createElements(tags, parent = null) {
     if (typeof tags === 'string') {
       const node = document.createElement(tags);
@@ -34,6 +36,8 @@ class PopUp {
     name.innerText = 'Pikachu';
     weight.innerText = '60 lbs';
 
+    this.loadComments(body);
+
     const close = this.createElements('button', section);
     close.innerText = 'X';
     close.addEventListener('click', () => {
@@ -50,6 +54,49 @@ class PopUp {
 
       document.querySelector('body').removeChild(popUp);
     }
+  }
+
+  static async loadComments(body) {
+    const div = this.createElements('div', body);
+
+    const h3 = this.createElements('h3', div);
+    h3.innerText = 'Comments (2)';
+
+    const id = 'item1';
+
+    const comments = await this.getComments(id);
+
+    comments.forEach((commentObj) => {
+      const { creation_date: date, username, comment } = commentObj;
+      const node = this.createElements('p', div);
+      node.classList.add('comment');
+      node.innerText = `${date}  ${username}: ${comment}`;
+    });
+  }
+
+  static async getComments(id) {
+    const response = await fetch(`${this.url}?item_id=${id}`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    const json = await response.json();
+    return json;
+  }
+
+  static async postComments(data) {
+    const response = await fetch(this.url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    const text = await response.text();
+    return text;
   }
 }
 
